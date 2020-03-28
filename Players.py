61a5ce77603b1feb1ceab17617e161d25f04a1cc
0N3IR0S <8enibue@gmail.com>
2020-03-28 23:15:53 +0100

    classes Players; move Players

---
+++ Players.py
@@ -0,0 +1,7 @@
+class Player:
+    def __init__(self, name, up, down, pos, score):
+        self.name = name
+        self.up = up
+        self.down = down
+        self.pos = pos
+        self.score = score
--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # 28.03.2020
 
 import pygame
+from Players import Player
 
 # initialize  pygame
 pygame.init()
@@ -15,9 +16,11 @@ pygame.display.set_caption("Pong2.0 by Me")
 icon = pygame.image.load("pongIcon.png")
 pygame.display.set_icon(icon)
 
-lead_x = 300
-lead_y = 300
 lead_x_change = 0
+
+# object setup Player
+Player1 = Player("Player1", False, False, 275, 0)
+Player2 = Player("Player2", False, False, 275, 0)
 
 # FPS limitation
 clock = pygame.time.Clock()
@@ -26,18 +29,58 @@ clock = pygame.time.Clock()
 game_exit = False
 while not game_exit:
     for event in pygame.event.get():
+        # Quit Program
         if event.type == pygame.QUIT:
             game_exit = True
+        # Key pressed
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                lead_x_change = -10
-                print("Hello")
-            if event.key == pygame.K_RIGHT:
-                lead_x_change = 10
+            # Player1
+            if event.key == pygame.K_w:
+                Player1.up = True
+            if event.key == pygame.K_s:
+                Player1.down = True
+            # Player2
+            if event.key == pygame.K_UP:
+                Player2.up = True
+            if event.key == pygame.K_DOWN:
+                Player2.down = True
+        # Key released
+        if event.type == pygame.KEYUP:
+            # Player1
+            if event.key == pygame.K_a:
+                Player1.up = False
+            if event.key == pygame.K_b:
+                Player1.down = False
+            # Player2
+            if event.key == pygame.K_UP:
+                Player2.up = False
+            if event.key == pygame.K_DOWN:
+                Player2.down = False
 
-    lead_x += lead_x_change
+    # move Player2
+    if Player2.up:
+        Player2.pos -= 15
+    else:
+        Player2.pos = Player2.pos
+
+    if Player2.down:
+        Player2.pos += 15
+    else:
+        Player2.pos = Player2.pos
+
+    # move Player2
+    if Player2.up:
+        Player2.pos -= 15
+    else:
+        Player2.pos = Player2.pos
+
+    if Player2.down:
+        Player2.pos += 15
+    else:
+        Player2.pos = Player2.pos
+
     game_screen.fill((0, 0, 0))
-    pygame.draw.rect(game_screen, (255, 255, 255), [lead_x, lead_y, 10, 10])
+    pygame.draw.rect(game_screen, (255, 255, 255), [770, Player2.pos, 10, 50])
     pygame.display.update()
 
     clock.tick(30)
